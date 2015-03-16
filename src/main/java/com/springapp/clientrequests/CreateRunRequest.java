0e04035a312de99efebb218b261e866cf8bcfc70
smoelge <smoelgebroed@gmail.com>
2015-03-16 13:44:10 +0100

    added file upload constraints and error handling. also prepared dat run POST

---
+++ src/main/java/com/springapp/clientrequests/CreateRunRequest.java
@@ -0,0 +1,76 @@
+package com.springapp.clientrequests;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.sql.Timestamp;
+
+/**
+ * Created by franschl on 16.03.15.
+ */
+
+/* The client wants to create a run.
+The information he gives:
+    - gpx-file
+    - distance
+    - own identity
+    - duration
+    - score
+    - timestamp
+ */
+
+public class CreateRunRequest {
+    MultipartFile gpxFile;
+    int distance;
+    int userID;
+    double duration;
+    double score;
+    Timestamp timestamp;
+
+    public MultipartFile getGpxFile() {
+        return gpxFile;
+    }
+    
+    public void setGpxFile(MultipartFile gpxFile) {
+        this.gpxFile = gpxFile;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+}
--- src/main/java/com/springapp/controllers/RunController.java
+++ src/main/java/com/springapp/controllers/RunController.java
@@ -1,18 +1,18 @@
 package com.springapp.controllers;
 
 import com.springapp.hibernate.RunsEntity;
+import org.apache.commons.fileupload.FileUploadException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * Created by franschl on 28.02.15.
@@ -22,20 +22,35 @@ public class RunController {
 
     @RequestMapping(value = "/user/{id}/run", method = RequestMethod.POST)
     //first only take care of file upload, then everything else
-    public ResponseEntity<RunsEntity> createRun(@RequestParam MultipartFile file) {
-        if (file.isEmpty()) {
-            return new ResponseEntity<RunsEntity>(HttpStatus.BAD_REQUEST);
-        }
+    public ResponseEntity<RunsEntity> createRun(@RequestParam MultipartFile file, @PathVariable("id") int groupID) {
         try {
-            byte[] bytes = file.getBytes();
-            //TODO change this to proper locationg
-            BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File("/home/franschl/Documents/Studienarbeit/uploadTest/uploaded.jpg")));
-            stream.write(bytes);
-            stream.close();
+            uploadFile(file);
         } catch(IOException e) {
             e.printStackTrace();
             return new ResponseEntity<RunsEntity>(HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (FileUploadException e) {
+            // File is empty
+            return new ResponseEntity<RunsEntity>(HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<RunsEntity>(HttpStatus.OK);
+    }
+
+    public String uploadFile(MultipartFile file) throws IOException, FileUploadException {
+        String dirPath = "/home/franschl/Documents/Studienarbeit/uploadTest/";
+        String fileName = String.valueOf(System.currentTimeMillis()) + ".gpx";
+        if (file.isEmpty()) {
+            throw new FileUploadException();
+        }
+        byte[] bytes = file.getBytes();
+
+        //file name collision handling, though it is very unlikely
+        while (Files.exists(Paths.get(dirPath + fileName))) {
+            fileName = String.valueOf(System.currentTimeMillis()) + ".gpx";
+        }
+
+        BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File(dirPath + fileName)));
+        stream.write(bytes);
+        stream.close();
+        return dirPath + fileName;
     }
 }
