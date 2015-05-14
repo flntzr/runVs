3d180d531bbe64b6e9101c6e9287e6b13ae4bf79
smoelge <smoelgebroed@gmail.com>
2015-05-14 18:17:37 +0200

    Added drawer, no actual content yet

---
+++ app/src/main/java/winzer/ghostrunner_app_final/Activities/Drawer/TestDrawerEntry.java
@@ -0,0 +1,9 @@
+package winzer.ghostrunner_app_final.Activities.Drawer;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by franschl on 5/14/15.
+ */
+public class TestDrawerEntry extends Fragment{
+}
