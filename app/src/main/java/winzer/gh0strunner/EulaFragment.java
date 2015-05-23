9c47d4a2f96a6f8ba9efa73997c810202f373eb9
smoelge <smoelgebroed@gmail.com>
2015-05-23 21:50:37 +0200

    Added license and information about us

--- app/src/main/java/winzer/gh0strunner/EulaFragment.java
+++ app/src/main/java/winzer/gh0strunner/EulaFragment.java
@@ -3,9 +3,11 @@ package winzer.gh0strunner;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by franschl on 5/23/15.
@@ -25,6 +27,11 @@ public class EulaFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_eula, container, false);
+
+        // make Link in gpl clickable
+        TextView aboutText = (TextView) rootView.findViewById(R.id.eula_text);
+        aboutText.setMovementMethod(LinkMovementMethod.getInstance());
+
         return rootView;
     }
 
