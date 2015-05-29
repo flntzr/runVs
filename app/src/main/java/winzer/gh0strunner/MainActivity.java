4e557799056d208990302a0a5c92f79dd0e34aaf
smoelge <smoelgebroed@gmail.com>
2015-05-29 16:35:20 +0200

    added groupview medals. reduced redundancy. reduced redundancy. added add_group_member button, applied ugly colors

--- app/src/main/java/winzer/gh0strunner/MainActivity.java
+++ app/src/main/java/winzer/gh0strunner/MainActivity.java
@@ -120,8 +120,8 @@ public class MainActivity extends Activity
             // Only show items in the action bar relevant to this screen
             // if the drawer is not showing. Otherwise, let the drawer
             // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.main, menu);
             restoreActionBar();
+            getMenuInflater().inflate(R.menu.main, menu);
             return true;
         }
         return super.onCreateOptionsMenu(menu);
